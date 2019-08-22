42cc9e102f3a9fbf8ab8fbbb5d52ec7ba029be06
Caina <cainacc@hotmail.com>
2019-08-22 09:22:01 -0300

    problema 5

--- HaskellListas/problema5.hs
+++ HaskellListas/problema5.hs
@@ -4,3 +4,16 @@ Crie uma fun ̧c ̃ao com assinaturabusca ::  [Int] -> Int -> Bool,
  passado como parˆametro encontra-se na lista ou n ̃ao.N ̃ao
   utilizenenhumafun ̧c ̃ao pronta do Haskell para realizar esta tarefa.
 -}
+
+busca ::  [Int] -> Int -> Bool
+busca [] _ = False
+busca (a:b) num = (num == a) || (busca b num)
+{-
+  if num == a then
+    True
+  else
+    busca b num
+-}
+
+main = do
+  print( (busca [2,6,5,3] 9) )
