cb0428d7540f50bf7ae7368c35df943ec07e020a
Caina <cainacc@hotmail.com>
2019-08-25 22:57:51 -0300

    problema11

--- HaskellBasicoPIV/problema11.hs
+++ HaskellBasicoPIV/problema11.hs
@@ -2,3 +2,32 @@
 Crie uma fun ̧c ̃ao que receba dois n ́umeros x e y e retorne o m ́aximo divisor
  comum (DICA: pesquise sobreo Algoritmo de Euclides).  Leia x e y do teclado.
 -}
+
+maior:: Int->Int->Int
+maior m n =
+  if m > n then
+    m
+  else
+    n
+
+menor :: Int->Int->Int
+menor m n =
+  if m < n then
+    m
+  else
+    n
+
+euclides :: Int->Int->Int
+euclides a 0 = a
+euclides a b = euclides b (mod a b)
+
+main = do
+  print("digite um valor a")
+  xString <- getLine
+  let x = (read xString :: Int)
+  print("digite um valor  b")
+  yString <- getLine
+  let y = (read yString :: Int)
+
+  print("O MDC eh: ")
+  print((euclides x y))
