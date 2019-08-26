c57148ff2f7f813ec8c77a7d69bd9300bc551665
Caina C. Caldas <cainacc@hotmail.com>
2019-08-26 16:52:34 -0300

    problema16

--- HaskellBasicoPIV/problema16.hs
+++ HaskellBasicoPIV/problema16.hs
@@ -1,4 +1,20 @@
 {-
-Crie uma fun ̧c ̃ao que receba dois n ́umeros x e y e retorne se x  ́e divis ́ıvel
+Crie uma funcao que receba dois numeros x e y e retorne se x ́e divisıvel
  por y.  Leia x e y do teclado.
 -}
+
+divisivel:: Int -> Int -> String
+divisivel a b =
+  if ( (mod a b) == 0) then
+    "a eh divisivel por b"
+  else
+    "a nao eh divisivel por b"
+
+main = do
+  print("digite um valor a")
+  xString <- getLine
+  let x = (read xString :: Int)
+  print("digite um valor  b")
+  yString <- getLine
+  let y = (read yString :: Int)
+  print( (divisivel x y) )
