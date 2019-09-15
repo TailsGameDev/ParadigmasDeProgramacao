f270f12591ff232559b1c912ad08e42ebaa944b7
Caina <cainacc@hotmail.com>
2019-09-15 16:03:53 -0300

    4 matrizes operantes ^^

--- T1/main.hs
+++ T1/main.hs
@@ -21,24 +21,14 @@ _  1 2 _
    _ _
 -}
 
+p0 = [1]
+m0 = [0, 1, 0,
+      1, o, 1,
+      0, 1, 0]
+r0 = [0, 1, 0,
+      1, 1, 1,
+      0, 1, 0]
 
-{-}
-possiveis = [1]
-maiorPossivel = 1
-matriz = [0, 1, 0, --o programa resolveu essa matriz corretamente
-          1, 1, 1,
-          0, 1, 0]
-
-
-
-possiveis = [1,2]
-maiorPossivel = 2
-matriz = [0, 2, 1, 0, --o programa resolveu essa matriz corretamente
-          2, 1, 2, 1,
-          1, 2, 1, 2,
-          0, 1, 2, 0]
-
--}
 p1 = [1,2]
 m1 = [0, 2, 1, 0,
       2, o, o, 1,
@@ -49,55 +39,54 @@ r1 = [0, 2, 1, 0,
       1, 2, 1, 2,
       0, 1, 2, 0]
 
-
-{-}
-p1 = [1,2]
-mp1 = 2
-m1 = [0, 1, 2, 0, --o programa resolveu essa matriz corretamente
-      1, o, o, 2,
+p2 = [1,2]
+m2 = [0, 2, 1, 0,
       2, o, o, 1,
-      0, 2, 1, 0]
--}
+      1, o, o, 2,
+      0, 1, 2, 0]
+r2 = [0, 2, 1, 0,
+      2, 1, 2, 1,
+      1, 2, 1, 2,
+      0, 1, 2, 0]
+
+p3 = [1,2,3]
+m3 = [0, 2, 2, 1, 0,
+      3, o, o, o, 1,
+      1, o, o, o, 2,
+      2, o, o, o, 2,
+      0, 2, 1, 3, 0]
+r3 = [0, 2, 2, 1, 0,
+      3, 1, 2, 3, 1,
+      1, 3, 1, 2, 2,
+      2, 2, 3, 1, 2,
+      0, 2, 1, 3, 0]
+t3 = [0, 2, 2, 1, 0,
+      3, 1, 2, 3, 1,
+      1, 3, 1, 2, 2,
+      2, o, o, o, 2,
+      0, 2, 1, 3, 0]
+
+
+p4 = [0,1,2,3]
+m4 = [0, 2, 2, 1, 2, 0,
+      2, o, o, o, o, 2,
+      2, o, o, o, o, 2,
+      3, o, o, o, o, 1,
+      1, o, o, o, o, 3,
+      0, 1, 2, 3, 1, 0]
+r4 = [0, 2, 2, 1, 2, 0,
+      2, 0, 1, 3, 2, 2,
+      2, 2, 3, 0, 1, 2,
+      3, 1, 0, 2, 3, 1,
+      1, 3, 2, 1, 0, 3,
+      0, 1, 2, 3, 1, 0]
+
+--tam = tamanhoLinha matriz
+tam m = tamanhoLinha m
 
 --maior possivel!!
 mp :: [Int] -> Int
 mp p = p!!( (length p) -1)
-
-{-}
-matriz = [0, 1, 2, 0, --o programa resolveu essa matriz corretamente
-          1, 2, 1, 2,
-          2, 1, 2, 1,
-          0, 2, 1, 0]
-
-possiveis = [1,2,3]
-maiorPossivel = 3
-matriz = [0, 2, 2, 1, 0, -- resolvei essa matriz corretamente!
-          3, 0, 0, 0, 1,
-          1, 0, 0, 0, 2,
-          2, 0, 0, 0, 3,
-          0, 2, 1, 3, 0]
--}
-{-
-possiveis = [1,2,3]
-maiorPossivel = 3
-matriz = [0, 2, 2, 1, 0,
-          3, 1, 2, 3, 1,
-          1, 3, 1, 2, 2,
-          2, 2, 3, 1, 3,
-          0, 2, 1, 3, 0]
-
-
-possiveis = [0,1,2,3]
-maiorPossivel = 3
-matriz = [0, 2, 2, 1, 2, 0, -- essa matriz ele deixa tudo zero
-          2, o, o, o, o, 2,
-          2, o, o, o, o, 2,
-          3, o, o, o, o, 1,
-          1, o, o, o, o, 3,
-          0, 1, 2, 3, 1, 0]
--}
---tam = tamanhoLinha matriz
-tam m = tamanhoLinha m
 
 infinito = 999999999
 inf = infinito
@@ -178,25 +167,32 @@ quantosVejo x y iter maiorVisto deslocX deslocY limX limY m
   | (getxym x y m) > maiorVisto = -- viu um predio maior e aumenta a contagem
       quantosVejo (x+deslocX) (y+deslocY) (iter+1) (getxym x y m) deslocX deslocY limX limY m
   | (getxym x y m) <= maiorVisto = -- predio eh igual (0) dae continua lendo mas nao incrementa contagem
-      quantosVejo (x+deslocX) (y+deslocY)  (iter)  (getxym x y m) deslocX deslocY limX limY m
+      quantosVejo (x+deslocX) (y+deslocY)  (iter)    maiorVisto   deslocX deslocY limX limY m
   -- | otherwise = iter --resultado encontrado por que o predio eh menor que o anterior
 
 
 vejoCerto :: Int -> Int -> [Int] -> Bool
 vejoCerto x y m =
-  ((getxym    0       y    m) >= quantosVejo    1       y    0 0   1    0   ((tam m)-1)   inf    m) && --esqParaDir
+  ((getxym    0       y    m) >=                                     quantosVejo    1         y      0 0   1    0   ((tam m)-1)   inf      m) && --esqParaDir
   --dirParaEsq -- equivalenca ( se p -> q) para (not p || q), ou seja, se x==tam-1 então linha deve ser valida. pois antes nao dah pra saber ao certo
-  ( ( not (x == ((tam m)-2)) ) || ((getxym ((tam m)-1)    y    m) >= quantosVejo ((tam m)-2)    y    0 0 (-1)   0      0      inf    m) ) &&
-  ((getxym    x       0    m) >= quantosVejo    x       1    0 0   0    1     inf   ((tam m)-1)  m) && --cimaParaBaixo
+  (( not (x == ((tam m)-2)) ) || ((getxym ((tam m)-1)    y    m)  >= quantosVejo ((tam m)-2)  y      0 0 (-1)   0      0          inf      m) ) &&
+  ((getxym    x       0    m) >=                                     quantosVejo    x         1      0 0   0    1     inf     ((tam m)-1)  m) && --cimaParaBaixo
   --baixoPraCima equivalencia tambem.. se y==tam-1 então verificar de baixo pra cima
-  (( not (y == ((tam m)-2)) ) || ((getxym    x    ((tam m)-1) m) >= quantosVejo    x    ((tam m)-2) 0 0   0  (-1)    inf      0     m) )
+  (( not (y == ((tam m)-2)) ) || ((getxym    x    ((tam m)-1) m)  >= quantosVejo    x    ((tam m)-2) 0 0   0  (-1)    inf          0       m) )
+
+testaVejoCerto :: Int -> Int -> [Int] -> IO()
+testaVejoCerto x y m = do
+  putStr( "\nEsqPAraDir: "++show(((getxym   0       y    m) >=                                     quantosVejo    1         y      0 0   1    0   ((tam m)-1)   inf      m)) )
+  putStr( "\nDirParaEsq: "++show(( not (x == ((tam m)-2)) ) || ((getxym ((tam m)-1)    y    m)  >= quantosVejo ((tam m)-2)  y      0 0 (-1)   0      0          inf      m) ) )
+  putStr( "\ncimapBaixo: "++show((getxym    x       0    m) >=                                     quantosVejo    x         1      0 0   0    1     inf     ((tam m)-1)  m))
+  putStr( "\nbaixopCima: "++show(( not (y == ((tam m)-2)) ) || ((getxym    x    ((tam m)-1) m)  >= quantosVejo    x    ((tam m)-2) 0 0   0  (-1)    inf          0       m) ))
 
 testaQtd :: Int->Int->[Int]->IO ()
 testaQtd x y m = do
-  putStr( "\nesq p/ dir:"++   show(quantosVejo    1       y    0 0   1    0   ((tam m)-1)   inf    m) ) --esq para dir
-  putStr( "\ndir p/ esq:"++   show(quantosVejo ((tam m)-2)    y    0 0 (-1)   0      0      inf    m) ) --dir para esq
-  putStr( "\ncima p/ baixo:"++show(quantosVejo    x       1    0 0   0    1     inf   ((tam m)-1)  m) ) --cima para baixo
-  putStr( "\nbaixo p/ cima:"++show(quantosVejo    x    ((tam m)-2) 0 0   0  (-1)    inf      0     m) ) --baixo para cima
+  putStr( "\nesq p/ dir:"++   show(quantosVejo    1         y      0 0   1    0  ((tam m)-1)   inf      m) ) --esq para dir
+  putStr( "\ndir p/ esq:"++   show(quantosVejo ((tam m)-2)  y      0 0 (-1)   0      0         inf      m) ) --dir para esq
+  putStr( "\ncima p/ baixo:"++show(quantosVejo    x         1      0 0   0    1     inf    ((tam m)-1)  m) ) --cima para baixo
+  putStr( "\nbaixo p/ cima:"++show(quantosVejo    x    ((tam m)-2) 0 0   0  (-1)    inf         0       m) ) --baixo para cima
 
 tahOk :: Int -> Int -> [Int] -> Bool
 tahOk x y m = (vejoCerto x y m) && (not (jaTemNaLinha (getxym x y m) 1 y (setXY (o) x y m) ) ) && (not (jaTemNaColuna (getxym x y m) x 1 (setXY (o) x y m)) )
@@ -251,7 +247,7 @@ resolve k x y m v p -- k= limiteDaRecursao, x, y, m, v=matrizGuardaIndexNoVetorD
 testahOk :: Int->Int->[Int]->IO ()
 testahOk x y m = do
   putStr( "numero: "++show(getxym x y m)++" posicao: ("++show(x)++","++show(y)++")\n")
-  putStr( "tahOk: " ++ show(tahOk 2 1 m) )
+  putStr( "tahOk: " ++ show(tahOk x y m) )
   putStr( "\nvejo certo:"++   show(vejoCerto x y m) )
   -- esse set de -1(o) eh porque se jah tiver o numero desejado na posição, ele vai considerar uma repetição.
   -- Então temos que passar uma matriz que não tem o numero desejado na posição alvo
@@ -270,14 +266,15 @@ resolvida :: [Int] -> [Int] -> [Int]
 resolvida p m = (resolve lim 1 1 m (zeros m) p)
 
 main = do
+  putStr("Assert resolve m0: " ++ show( resolvida p0 m0 == r0 )++"\n")
   putStr("Assert resolve m1: " ++ show( resolvida p1 m1 == r1 )++"\n")
-  --putStr( (showMatriz 0 0 m1) )
-  --print( (possiveis!!(getxym 1 1 (zeros matriz) )) )
-  --resolveEntre 10000 10000 p1 m1
-  --resolveEntre 1 10
-  --testahOk 2 1 matriz
-  --putStr( (showMatriz 0 0 (setXY (possiveis!!(getxym 1 1 (zeros matriz))) 1 1 matriz )) )
-  --print( (possiveis!!(getxym 1 1 (zeros matriz))) )
-  --print( (tahOk 1 1 (setXY (possiveis!!(getxym 1 1 (zeros matriz))) 1 1 matriz ) ) )
-  --print( (vejoCerto 2 1 matriz) )
-  --testaQtd 4 1 matriz
+  putStr("Assert resolve m2: " ++ show( resolvida p2 m2 == r2 )++"\n")
+  putStr("Assert resolve m3: " ++ show( resolvida p3 m3 == r3 )++"\n")
+  putStr("Assert resolve m4: " ++ show( resolvida p4 m4 == r4 )++"\n")
+  --putStr( "inicial: \n"++(showMatriz 0 0 m3) )
+  --putStr( "\nresult: \n"++(showMatriz 0 0 r3) )
+  --resolveEntre 20 30 p3 m3
+  --putStr( "\nresult: \n"++(showMatriz 0 0 t3) )
+  --testahOk 3 2 t3
+  --testaQtd 3 2 t3
+  --testaVejoCerto 3 2 t3
