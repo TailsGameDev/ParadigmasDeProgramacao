a480806977d0d59e22abd37aecb31dc5f4ad68a6
Vinicius <vinicius_gs_lp@hotmail.com>
2019-11-21 09:42:19 -0200

    Quantos vejo

--- T3/wolkenkratzer.pl
+++ T3/wolkenkratzer.pl
@@ -5,20 +5,19 @@ sudoku(Rows) :-
         append(Rows, Vs), Vs ins 1..Length,
         maplist(all_distinct, Rows),
         transpose(Rows, Columns),
-        maplist(all_distinct, Columns),
-        Rows = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
-        blocks(As, Bs, Cs),
-        blocks(Ds, Es, Fs),
-        blocks(Gs, Hs, Is).
+        maplist(all_distinct, Columns).
 
-blocks([], [], []).
-blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
-        all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
-        blocks(Ns1, Ns2, Ns3).
+quantosVejo([], _, 0).
+quantosVejo([H|T], Max, V) :- H > Max, quantosVejo(T, H, V1), V is V1 + 1, !.
+quantosVejo([_|T], Max, V1) :- quantosVejo(T, Max, V1).
 
-quantosVejo([], 0).
-quantosVejo([H,T], Max, V) :- H > Max, quantosVejo(T, H, V1), V is V1 + 1.
-quantosVejo([_,T], Max, V1) :- quantosVejo(T, Max, V1).
+inverte([], []).
+inverte([H|T], I) :- inverte(T, I1), append(I1, [H], I).
+
+vejoCerto(Lista, V) :- quantosVejo(Lista, -1, V).
+
+soma([], 0).
+soma([H|T], S) :- soma(T, S1), S is S1 + H.
 
 problem(1, [[_,_,_,_,_,_,_,_,_],
             [_,_,_,_,_,3,_,8,5],
