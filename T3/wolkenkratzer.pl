6fec9dd8a5f71298e5b75ddf281f22605156f812
Vinicius <vinicius_gs_lp@hotmail.com>
2019-11-21 10:24:37 -0200

    vejoCerto funciona

--- T3/wolkenkratzer.pl
+++ T3/wolkenkratzer.pl
@@ -6,17 +6,20 @@ sudoku(Rows) :-
         maplist(all_distinct, Rows),
         transpose(Rows, Columns),
         maplist(all_distinct, Columns),
-        Rows = [_,Bs,_,_,_,_,_,_,_],
-        quantosVejo(Bs, -1, V), V #= 5.
+        % Rows = [_,Bs,_,_,_,_,_,_,_],
+        % vejoCerto(Bs, 5).
 
-quantosVejo([], _, 0).
-quantosVejo([H|T], Max, V) :- H > Max, quantosVejo(T, H, V1), V is V1 + 1, !.
-quantosVejo([_|T], Max, V1) :- quantosVejo(T, Max, V1).
+quantosVejo([], 0).
+quantosVejo(Lista, V) :- quantosVejoRecursivo(Lista, -1, V).
+
+quantosVejoRecursivo([], _, 0).
+quantosVejoRecursivo([H|T], Max, V) :- H #> Max, quantosVejoRecursivo(T, H, V1), V is V1 + 1, !.
+quantosVejoRecursivo([_|T], Max, V1) :- quantosVejoRecursivo(T, Max, V1).
 
 inverte([], []).
 inverte([H|T], I) :- inverte(T, I1), append(I1, [H], I).
 
-% vejoCerto(Lista, V) :- quantosVejo(Lista, -1, V).
+vejoCerto(Lista, V) :- quantosVejo(Lista, V).
 
 soma([], 0).
 soma([H|T], S) :- soma(T, S1), S is S1 + H.
@@ -30,6 +33,9 @@ problem(1, [[_,_,_,_,_,_,_,_,_],
             [5,_,_,_,_,_,_,7,3],
             [_,_,2,_,1,_,_,_,_],
             [_,_,_,_,4,_,_,_,9]]).
+
+% ?- vejoCerto([A,B,C,D,E], 4), A in 1..5, B in 1..5, C in 1..5, D in 1..5, E in 1..5, all_distinct([A,B,C,D,E]), label([A,B,C,D,E]).
+
 
 % [9, 8, 7, 6, 5, 4, 3, 2, 1].
 % [2, 4, 6, 1, 7, 3, 9, 8, 5].
