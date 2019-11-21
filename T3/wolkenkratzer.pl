8a1cedf3aa5eb559a05dbf18eaeac95850034de9
Vinicius <vinicius_gs_lp@hotmail.com>
2019-11-21 09:14:05 -0200

    Criei o arquivo

---
+++ T3/wolkenkratzer.pl
@@ -0,0 +1,31 @@
+:- use_module(library(clpfd)).
+
+sudoku(Rows) :-
+        length(Rows, Length), maplist(same_length(Rows), Rows),
+        append(Rows, Vs), Vs ins 1..Length,
+        maplist(all_distinct, Rows),
+        transpose(Rows, Columns),
+        maplist(all_distinct, Columns),
+        Rows = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
+        blocks(As, Bs, Cs),
+        blocks(Ds, Es, Fs),
+        blocks(Gs, Hs, Is).
+
+blocks([], [], []).
+blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
+        all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
+        blocks(Ns1, Ns2, Ns3).
+
+quantosVejo([], 0).
+quantosVejo([H,T], Max, V) :- H > Max, quantosVejo(T, H, V1), V is V1 + 1.
+quantosVejo([_,T], Max, V1) :- quantosVejo(T, Max, V1).
+
+problem(1, [[_,_,_,_,_,_,_,_,_],
+            [_,_,_,_,_,3,_,8,5],
+            [_,_,1,_,2,_,_,_,_],
+            [_,_,_,5,_,7,_,_,_],
+            [_,_,4,_,_,_,1,_,_],
+            [_,9,_,_,_,_,_,_,_],
+            [5,_,_,_,_,_,_,7,3],
+            [_,_,2,_,1,_,_,_,_],
+            [_,_,_,_,4,_,_,_,9]]).
